fdb9847d639f189ab015cf1a92a2db7894df9af5
yelinnoo-007 <yelinnoo222000@gmail.com>
2024-05-08 14:54:41 +0630

    create factories and test CountryTest

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,23 @@
 
 namespace Tests;
 
+use App\Models\Country;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected User $user;
+    protected Country $country;
+
+    protected function createAdmin(): User
+    {
+        return User::factory()->create();
+    }
+
+    protected function createCountry(): Country
+    {
+        return Country::factory()->create();
+    }
 }
