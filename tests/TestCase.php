58a2600bec70a225cff9655ccd7f55e7927fa3ca
yelinnoo-007 <yelinnoo222000@gmail.com>
2024-05-09 18:13:13 +0630

    test country api

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,6 +3,7 @@
 namespace Tests;
 
 use App\Models\Country;
+use App\Models\State;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
@@ -11,6 +12,7 @@ abstract class TestCase extends BaseTestCase
 {
     protected User $user;
     protected Country $country;
+    protected State $state;
 
     protected function createAdmin(): User
     {
@@ -20,5 +22,19 @@ abstract class TestCase extends BaseTestCase
     protected function createCountry(): Country
     {
         return Country::factory()->create();
+    }
+
+    protected function createState(): State
+    {
+        return State::factory()->create([
+            'country_id' => $this->country->id
+        ]);
+    }
+
+    protected function createCity()
+    {
+        // return City::factory()->create([
+        //     'state_id' => $this->country->id
+        // ]);
     }
 }
