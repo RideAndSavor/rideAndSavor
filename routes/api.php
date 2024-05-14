f0361ca9553cedf586b342f0697b9292cddd73ea
yelinnoo-007 <yelinnoo222000@gmail.com>
2024-05-14 14:32:14 +0630

    create user test

--- routes/api.php
+++ routes/api.php
@@ -23,7 +23,7 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::post('register', [AuthController::class, 'register']);
+Route::post('register', [AuthController::class, 'register'])->name('register');
 Route::post('login', [AuthController::class, 'login'])->name('login');
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 Route::resource('paymentmodes', PaymentProviderController::class);
