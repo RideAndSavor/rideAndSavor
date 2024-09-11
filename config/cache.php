63fecb811976151960730d7c581b1845f83da2e9
ThuzarMyint10 <thuzarmyint795@gmail.com>
2024-09-11 13:42:50 +0630

    deployment configuration

--- config/cache.php
+++ config/cache.php
@@ -40,9 +40,9 @@ return [
 
         'database' => [
             'driver' => 'database',
-            'table' => env('DB_CACHE_TABLE', 'cache'),
-            'connection' => env('DB_CACHE_CONNECTION'),
-            'lock_connection' => env('DB_CACHE_LOCK_CONNECTION'),
+            // 'table' => env('DB_CACHE_TABLE', 'cache'),
+            // 'connection' => env('DB_CACHE_CONNECTION'),
+            // 'lock_connection' => env('DB_CACHE_LOCK_CONNECTION'),
         ],
 
         'file' => [
