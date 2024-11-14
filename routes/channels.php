0963cb4091fdfd4b8fac2be5b749fda90411bd50
ThuzarMyint10 <thuzarmyint795@gmail.com>
2024-11-14 16:23:03 +0630

    check driver is online or offline

--- routes/channels.php
+++ routes/channels.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Facades\Broadcast;
 
+Broadcast::channel('online-users', function ($user) {
+    return [
+        'id' => $user->id,
+        'name' => $user->name,
+    ];
+});
+
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
